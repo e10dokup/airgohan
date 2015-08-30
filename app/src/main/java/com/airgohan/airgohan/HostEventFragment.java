c09693d9fb80820b9a5d08b44fd1d632a9492c62
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-08-30 14:48:47 +0900

    create put event

--- app/src/main/java/com/airgohan/airgohan/HostEventFragment.java
+++ app/src/main/java/com/airgohan/airgohan/HostEventFragment.java
@@ -4,10 +4,22 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.airgohan.airgohan.model.Event;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,6 +31,23 @@ public class HostEventFragment extends Fragment {
 
     @Bind(R.id.btn_create)
     Button mCreateButton;
+    @Bind(R.id.edit_name)
+    EditText mNameEdit;
+    @Bind(R.id.edit_address)
+    EditText mAddressEdit;
+    @Bind(R.id.edit_genre)
+    EditText mGenreEdit;
+    @Bind(R.id.edit_main_menu)
+    EditText mMainMenuEdit;
+    @Bind(R.id.edit_max_members)
+    EditText mMaxMembersEdit;
+    @Bind(R.id.edit_start_time)
+    EditText mStartTimeEdit;
+    @Bind(R.id.edit_finish_time)
+    EditText mFinishTimeEdit;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+    Event event = Utils.selectedEvent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,13 +60,34 @@ public class HostEventFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_host_event, container, false);
         ButterKnife.bind(this, view);
+
+        if(event != null){
+            mNameEdit.setText(event.getName());
+            mAddressEdit.setText(event.getAddress());
+            mGenreEdit.setText(event.getGenre());
+            mMainMenuEdit.setText(event.getMainMenu());
+            mMaxMembersEdit.setText(String.valueOf(event.getMaxMember()));
+            mStartTimeEdit.setText(sdf.format(event.getStartDate()));
+            mFinishTimeEdit.setText(sdf.format(event.getFinishDate()));
+        }
         return view;
     }
 
     @OnClick(R.id.btn_create)
     public void onCreatButtonClicked(){
-        FragmentManager manager = getFragmentManager();
-        manager.popBackStack();
+
+        if(event != null) {
+            try {
+                Event newEvent = new Event(event.getId(), event.getHostId(),
+                        mNameEdit.getText().toString(), mAddressEdit.getText().toString(),
+                        mGenreEdit.getText().toString(), mMainMenuEdit.getText().toString(),
+                        sdf.parse(mStartTimeEdit.getText().toString()), sdf.parse(mFinishTimeEdit.getText().toString()),
+                        Integer.parseInt(mMaxMembersEdit.getText().toString()));
+                newEvent.putEvents(getActivity(), mPutSuccessListener, mErrorListener);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -45,4 +95,21 @@ public class HostEventFragment extends Fragment {
         super.onAttach(activity);
 
     }
+
+    Response.Listener<JSONObject> mPutSuccessListener = new Response.Listener<JSONObject>() {
+        @Override
+        public void onResponse(JSONObject response) {
+            Log.d(TAG, response.toString());
+            FragmentManager manager = getFragmentManager();
+            manager.popBackStack();
+        }
+    };
+
+    private Response.ErrorListener mErrorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d(TAG, error.toString());
+            Toast.makeText(getActivity(), "何か問題があるよ", Toast.LENGTH_LONG).show();
+        }
+    };
 }
--- app/src/main/java/com/airgohan/airgohan/MainActivity.java
+++ app/src/main/java/com/airgohan/airgohan/MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
