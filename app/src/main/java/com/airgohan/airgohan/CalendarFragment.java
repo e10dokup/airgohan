0accf7455fb2b72eebabe0ae862e86a209cc4d76
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-08-30 13:13:55 +0900

    Implement transaction calendar → hostevent

--- app/src/main/java/com/airgohan/airgohan/CalendarFragment.java
+++ app/src/main/java/com/airgohan/airgohan/CalendarFragment.java
@@ -1,9 +1,11 @@
 package com.airgohan.airgohan;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,13 +17,17 @@ import com.airgohan.airgohan.model.Event;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
+import org.apache.http.impl.cookie.DateUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import butterknife.Bind;
@@ -33,17 +39,6 @@ public class CalendarFragment extends Fragment {
     CalendarView mCalendarView;
 
     Event mEvent = new Event();
-    List<Event> mEvents = new ArrayList<Event>();
-
-    // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
-        return fragment;
-    }
-
-    public CalendarFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,9 +51,9 @@ public class CalendarFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calender, container, false);
         ButterKnife.bind(this, view);
-        mCalendarView.setOnDateChangeListener(mOnDateChangeListener);
 
         mEvent.getHostEvents(getActivity(), 1, mSuccessListener, mErrorListener);
+        mCalendarView.setOnDateChangeListener(mOnDateChangeListener);
         
         return view;
     }
@@ -73,19 +68,37 @@ public class CalendarFragment extends Fragment {
     CalendarView.OnDateChangeListener mOnDateChangeListener = new CalendarView.OnDateChangeListener() {
         @Override
         public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-            Log.d("CalendaerFragment",String.valueOf(year) +String.valueOf(month+1) + String.valueOf(dayOfMonth));
+            for(Event event : Utils.events){
+//                Toast.makeText(getActivity(), String.valueOf(e.getStartDate().getTime()), Toast.LENGTH_LONG).show();
+                Date eventTrntDate = clearTime(event.getStartDate());
+                SimpleDateFormat sdf =  new SimpleDateFormat("yyyy-MM-dd");
+                Date selectedDate = new Date();
+                try{
+                    selectedDate = sdf.parse(String.valueOf(year) + "-" + String.valueOf(month+1) + "-" + String.valueOf(dayOfMonth));
+                }catch(ParseException e){
+                    e.printStackTrace();
+                }
+                if(selectedDate.equals(eventTrntDate)){
+                    Utils.selectedEvent = event;
+                    FragmentManager manager = getFragmentManager();
+                    FragmentTransaction transaction  = manager.beginTransaction();
+                    transaction.addToBackStack(null);
+                    transaction.replace(R.id.content_frame, new HostEventFragment());
+                    transaction.commit();
+                    break;
+                }
+            }
         }
     };
 
     private Response.Listener<JSONObject> mSuccessListener = new Response.Listener<JSONObject>() {
         @Override
         public void onResponse(JSONObject response) {
-            Log.d("CalenderFragment", response.toString());
             try{
                 JSONArray values = response.getJSONArray("records");
                 for(int i = 0; i < values.length(); i++){
                     JSONObject value = values.getJSONObject(i);
-                    mEvents.add(new Event(value));
+                    Utils.events.add(new Event(value));
                 }
             }catch(JSONException e){
                 Log.d("CalenderFragment", e.toString());
@@ -100,5 +113,17 @@ public class CalendarFragment extends Fragment {
             Toast.makeText(getActivity(), "何か問題があるよ", Toast.LENGTH_LONG).show();
         }
     };
+
+    static Date clearTime(Date date) {
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.clear(Calendar.AM_PM);
+        cal.clear(Calendar.HOUR);
+        cal.clear(Calendar.HOUR_OF_DAY);
+        cal.clear(Calendar.MINUTE);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+        return cal.getTime();
+    }
 
 }
